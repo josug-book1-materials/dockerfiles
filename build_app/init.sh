a9e5eea636e13ddc311fe66eca1d135341f693d0
Etsuji Nakai <enakai@redhat.com>
2014-09-13 22:08:56 +0900

    Fix: remove redundant cd command

--- build_app/init.sh
+++ build_app/init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
+
 cd /root/openstack-sample-app
 sed -i "s/^rest_host = .*/rest_host = $REST_PORT_5555_TCP_ADDR/" endpoint.conf
 sed -i "s/^db_host = .*/db_host = $DB_PORT_3306_TCP_ADDR/" endpoint.conf
-cd /root/openstack-sample-app/
 python rest.py >> rest.log &
 
 cat <<EOF >>~/.bashrc
