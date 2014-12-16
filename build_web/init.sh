27006fda28dab2b7a691b3e618f7dea962d898cc
Etsuji Nakai <enakai@redhat.com>
2014-12-17 00:13:59 +0900

    Fix: application path for app/web

--- build_web/init.sh
+++ build_web/init.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cd /root/openstack-sample-app
+cd /root/sample-app
 sed -i "s/^rest_host = .*/rest_host = $REST_PORT_5555_TCP_ADDR/" endpoint.conf
 sed -i "s/^db_host = .*/db_host = $DB_PORT_3306_TCP_ADDR/" endpoint.conf
-sh /root/openstack-sample-app/server-setup/web.init.sh start
+sh /root/sample-app/server-setup/web.init.sh start
 
 cat <<EOF >>~/.bashrc
-trap '/root/openstack-sample-app/server-setup/web.init.sh stop; exit 0' TERM
+trap '/root/sample-app/server-setup/web.init.sh stop; exit 0' TERM
 EOF
 exec /bin/bash
