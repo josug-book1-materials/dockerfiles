c095b265cdc4d0236185a1acb98d941947a3b905
Etsuji Nakai <enakai@redhat.com>
2014-12-27 22:05:01 +0900

    Change: app/web start/stop commands

--- build_app/init.sh
+++ build_app/init.sh
@@ -3,9 +3,9 @@
 cd /root/sample-app
 sed -i "s/^rest_host = .*/rest_host = $REST_PORT_5555_TCP_ADDR/" endpoint.conf
 sed -i "s/^db_host = .*/db_host = $DB_PORT_3306_TCP_ADDR/" endpoint.conf
-python rest.py >> rest.log &
+sh /root/sample-app/server-setup/rest.init.sh start
 
 cat <<EOF >>~/.bashrc
-trap 'pkill -f rest.py && sleep 1; exit 0' TERM
+trap 'sh /root/sample-app/server-setup/rest.init.sh stop; exit 0' TERM
 EOF
 exec /bin/bash
--- build_web/init.sh
+++ build_web/init.sh
@@ -6,6 +6,6 @@ sed -i "s/^db_host = .*/db_host = $DB_PORT_3306_TCP_ADDR/" endpoint.conf
 sh /root/sample-app/server-setup/web.init.sh start
 
 cat <<EOF >>~/.bashrc
-trap '/root/sample-app/server-setup/web.init.sh stop; exit 0' TERM
+trap 'sh /root/sample-app/server-setup/web.init.sh stop; exit 0' TERM
 EOF
 exec /bin/bash
